ab537242161e2df4a130ca5ba9e0548582a02c65
Anatolii Burtsev <anatolyburtsev@yandex.ru>
2020-12-25 16:01:24 -0800

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import _ from 'lodash';
 
 console.log('blah');
@@ -7,5 +8,7 @@ console.log(_.intersection([1, 2], [2, 3]));
 console.log(_.last(['one', 'two']));
 
 function sayHi(name) {
-    return 'Result: ' + name;
+  return `Result: ${name}`;
 }
+
+console.log(sayHi('pew'));
